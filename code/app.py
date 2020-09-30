23e83105f90ab4300d946981ad7da2f11cee8f2e
Leo <leoaguilarsanchez97@gmail.com>
2020-09-29 23:19:43 -0600

    Agregadas primeras funcionalidades del menu

--- code/app.py
+++ code/app.py
@@ -1,43 +1,246 @@
+import sys
+import time
+import random
+from warnings import catch_warnings
+from requests.sessions import Request
 import spacy
+import colored
+import names
+import requests
+
+from colored import stylize
 from consolemenu import *
+from consolemenu.format import *
 from consolemenu.items import *
 
-menu = ConsoleMenu("Sentencer", "Divisor de oraciones", exit_option_text='Salir', prologue_text='Hola!', epilogue_text='epilogo')
-menu_item = MenuItem("Menu Item")
+author_names = []
+data = []
+ip = ""
 
-function_item = FunctionItem("Call a Python function", input, ["Enter an input"])
-command_item = CommandItem("Run a console command",  "touch hello.txt")
+blue = colored.fg("dark_blue") + colored.bg("light_blue") + \
+    colored.attr("bold")
+magenta = colored.fg("light_magenta") + colored.attr("blink")
+green = colored.fg("dark_green") + \
+    colored.bg("light_green") + colored.attr("bold")
+red = colored.fg("dark_red_1") + colored.bg("light_red") + colored.attr("bold")
 
-selection_menu = SelectionMenu(["item1", "item2", "item3"])
 
-submenu_item = SubmenuItem("Submenu item", selection_menu, menu)
+def set_custom_boundaries(doc):
+    for token in doc[:-1]:
+        if token.text == "...":
+            doc[token.i+1].is_sent_start = True
+        if token.text == "–":
+            doc[token.i+1].is_sent_start = True
+    return doc
 
-menu.append_item(menu_item)
-menu.append_item(function_item)
-menu.append_item(command_item)
-menu.append_item(submenu_item)
 
-menu.show()
+nlp = spacy.load("es_core_news_sm")
+nlp.add_pipe(set_custom_boundaries, before="parser")
 
-def split_sentences(): 
-    text = ""
-    with open('../data.txt', 'r', encoding='utf-8') as file:
-        text = file.read().replace('\n', '')
 
-    nlp = spacy.load("es_core_news_sm")
+def print_data():
 
-    def set_custom_boundaries(doc):
-        for token in doc[:-1]:
-            if token.text == "...":
-                doc[token.i+1].is_sent_start = True
-            if token.text == "–":
-                doc[token.i+1].is_sent_start = True
-        return doc
+    print("")
+    print(stylize(" ORACIONES ", blue))
+    print("")
+    print(stylize(str(len(data)) +
+                  " oraciones encontradas y emparejadas con su autor", magenta))
 
-    nlp.add_pipe(set_custom_boundaries, before="parser")
+    print("")
+    print(stylize(" AUTORES ", blue))
+    print("")
+    print(stylize("Hay " + str(len(author_names)) + " emparejados...", magenta))
+
+    for author in author_names:
+        print("*", stylize(author, magenta))
+
+
+def progress(count, total, status=''):
+    bar_len = 50
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
+
+
+def leer_archivo():
+
+    print(stylize(" ABRIENDO ARCHIVO ", green))
+    print("")
+
+    while (True):
+        fname = ''
+
+        try:
+            print(stylize(" ELIGIENDO ARCHIVO ", green))
+            print("")
+            fname = Screen().input(">> Ingresar una ruta completa (sin comillas encerrandolo): ")
+            f = open(fname, 'r', encoding='utf-8')
+
+            print(stylize(" ARCHIVO ACEPTADO ", green))
+            print("")
+            with f:
+                text = f.read()
+
+            print(stylize(" << " + fname + " >> ",
+                          blue, colored.attr("underlined")))
+            print("")
+            print(text)
+            print(stylize(" << " + fname + " >>",
+                          blue, colored.attr("underlined")))
+            print("")
+
+            option = Screen().input(">> ¿Desea cambiar el archivo? (y/n): ")
+            if (option != "y"):
+                break
+
+        except OSError as e:
+            print(stylize(" ¡ERROR! ", red))
+            print(
+                stylize("El archivo [" + fname + "] no se puede procesar.", colored.fg("red")))
+            if hasattr(e, 'message'):
+                print(stylize(e.message, colored.fg("red_3a")))
+            else:
+                print(stylize(e, colored.fg("red_3a")))
+
+            Screen().input('Presiona [Enter] para continuar')
+
+            print("")
+            print("")
+
+    print("")
+    print(stylize(" SEPARANDO POR ORACIONES ", green))
+    print("")
+
     doc = nlp(text)
-
     sentences = [sent.text for sent in doc.sents]
+    time.sleep(1)
+    print(stylize(str(len(sentences)) + " oraciones encontradas", magenta))
 
-    print(sentences)
-    print(len(sentences))
+    print("")
+    print(stylize(" CARGANDO AUTORES ", green))
+    print("")
+
+    autors_length = random.randint(3, 10)
+    author_names.clear()
+
+    for x in range(autors_length):
+        author_name = names.get_full_name()
+        author_names.append(author_name)
+        progress(x, autors_length, status='Buscando un autor')
+        time.sleep(0.1)
+
+    progress(autors_length, autors_length, status='Autores agregados')
+    time.sleep(0.1)
+
+    sys.stdout.flush()
+    print("")
+    print(stylize(str(autors_length) + " autores asignados", magenta))
+
+    print("")
+    print(stylize(" SEPARANDO POR ORACIONES ", green))
+    print("")
+    i = 1
+    total = len(sentences)
+    data.clear()
+    for sent in sentences:
+        i = i + 1
+        data.append({"oracion": sent, "autor": random.choice(author_names)})
+        progress(i, total, status='Emparejando oraciones')
+        time.sleep(0.1)
+
+    progress(total, total, status='Oraciones emparejadas')
+
+    sys.stdout.flush()
+    print("")
+    print(stylize(" CARGA FINALIZADA ", green))
+    print("")
+
+    print_data()
+    print("")
+    Screen().input('Presiona [Enter] para continuar')
+
+
+def leer_ip():
+    global ip
+
+    print(stylize(" INTRODUCIR DIRECCION IP ", green))
+    print("")
+    print(stylize(" Ejemplo: http://127.0.0.1", magenta))
+    print("")
+
+    while True:
+        try:
+            ip = str(Screen().input(">> Dirección: ")).lower()
+            if not "http" in ip:
+                ip = "http://" + ip
+
+            break
+        except Exception:
+            print(stylize(" ERROR ", red))
+            print(stylize("Ingrese un IP válido", colored.fg("red")))
+
+        print("")
+        print(stylize(" INTRODUCIR DIRECCION IP ", green))
+
+    print("")
+
+    print(stylize(" COMPROBANDO LA DIRECCIÓN [" + ip + "] ", green))
+    print("")
+    try:
+        x = requests.get(ip)
+
+        print(stylize(" PETICION CORRECTA ", blue))
+        print(stylize("Respuesta: " + str(x.json), magenta))
+        print(stylize("Codigo: " + str(x.status_code), magenta))
+
+    except requests.exceptions.Timeout:
+        print(stylize(" ERROR ", red))
+        print(stylize("La conexion se tardo demasiado en responder", colored.fg("red")))
+
+    except requests.exceptions.RequestException as e:
+        print(stylize(" ERROR ", red))
+        print(stylize("Hubo un error con la peticion", colored.fg("red")))
+        if hasattr(e, 'message'):
+            print(stylize(e.message, colored.fg("red_3a")))
+        else:
+            print(stylize(e, colored.fg("red_3a")))
+
+    print("")
+    Screen().input('Presiona [Enter] para continuar')
+
+
+def create_menu():
+
+    menu = ConsoleMenu("Sentencer", "Asignador de autores y separador de oraciones", exit_option_text="Salir",
+                       prologue_text="Selecciona un archivo, se dividira en oraciones y se le asociariá un autor. Se convertirán en objetos que luego se enviarán a la dirección IP definida.",
+                       epilogue_text="Luis Leonel Aguilar Sánchez - 201603029 - SO1 2S2020",
+                       formatter=MenuFormatBuilder()
+
+                       .set_prompt("Selecciona>>")
+                       .set_title_align('center')
+                       .set_subtitle_align('center')
+                       .set_border_style_type(MenuBorderStyleType.DOUBLE_LINE_BORDER)
+                       .show_prologue_top_border(True)
+                       .show_epilogue_top_border(True)
+                       .show_prologue_bottom_border(True))
+
+    read_file_item = FunctionItem("Ingresar ruta del archivo", leer_archivo)
+    read_ip_item = FunctionItem("Ingresar dirección del load-balancer", leer_ip)
+
+    menu.append_item(read_file_item)
+    menu.append_item(read_ip_item)
+
+    # Show the menu
+    menu.start()
+    menu.join()
+
+
+if __name__ == '__main__':
+    for x in range(10):
+        author_names.append(names.get_full_name())
+
+    create_menu()
