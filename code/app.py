241e0b834bc9bff7938abf0d12337f34ee65d53c
Leo <leoaguilarsanchez97@gmail.com>
2020-09-30 03:24:35 -0600

    Finalizada primera version del cliente de python

--- code/app.py
+++ code/app.py
@@ -1,13 +1,14 @@
+from re import error
 import sys
 import time
 import random
-from warnings import catch_warnings
 from colored import colors
 from requests.sessions import Request
 import spacy
 import colored
 import names
 import requests
+import json
 
 from terminaltables import DoubleTable
 from colored import stylize
@@ -20,6 +21,7 @@ all_author_colors = [163, 154, 9, 4, 1, 13, 17, 18, 22, 23, 25, 38, 40, 50, 52, 
 
 data = []
 ip = ""
+url = "/data"
 
 blue = colored.fg("dark_blue") + colored.bg("light_blue") + \
     colored.attr("bold")
@@ -86,7 +88,8 @@ def leer_archivo():
             print("")
             fname = Screen().input(">> Ingresar una ruta completa (sin comillas encerrandolo): ")
             if (fname == ""):
-                fname = "C:\\Users\\leoag\\OneDrive\\Documents\\Universidad\\SOPES1\\Proyecto 1\\texto3.txt"
+                #fname = "C:\\Users\\leoag\\OneDrive\\Documents\\Universidad\\SOPES1\\Proyecto 1\\texto3.txt"
+                fname = "../data.txt"
             f = open(fname, 'r', encoding='utf-8')
 
             print(stylize(" ARCHIVO ACEPTADO ", green))
@@ -174,6 +177,7 @@ def leer_archivo():
 
 def leer_ip():
     global ip
+    global url
 
     print(stylize(" INTRODUCIR DIRECCION IP ", green))
     print("")
@@ -233,9 +237,30 @@ def leer_ip():
             print(stylize(e, colored.fg("red_3a")))
 
     print("")
+
+    print(stylize(" INTRODUCIR URL AL QUE DEBE HACER POST ", green))
+    print("")
+    print(stylize(" Ejemplo-Default: /data", magenta))
+    print("")
+
+    tmp_url = str(Screen().input(">> Url: ")).lower()
+    
+    if tmp_url != "" and not "/" in tmp_url:
+        tmp_url = "/" + tmp_url
+
+    url = url if tmp_url == "" else tmp_url
+
+    print(stylize(" SE UTILIZARÁ EL URL [" + url + "] ", blue))
+    print("")
+    
     Screen().input('Presiona [Enter] para continuar')
 
 def table_data():
+    if (len(data) == 0):
+        print(stylize(" POR FAVOR INGRESA LOS DATOS DEL ARCHIVO ", orange))
+        print("")
+        leer_archivo()
+
     sys.stdout.flush()
     print("")
     print(stylize(" DATOS RECOLECTADOS DEL ARCHIVO ", green))
@@ -266,6 +291,14 @@ def table_data():
     Screen().input('Presiona [Enter] para continuar')
 
 def list_data():
+    if (len(data) == 0):
+        print(stylize(" POR FAVOR INGRESA LOS DATOS DEL ARCHIVO ", orange))
+        print("")
+        leer_archivo()
+
+    sys.stdout.flush()
+    print("")
+
     i = 1
     biggest = 0
     for row in data: 
@@ -294,6 +327,80 @@ def list_data():
     print("")
     Screen().input('Presiona [Enter] para continuar')
 
+def post_data():
+    if (ip == ""):
+        print(stylize(" POR FAVOR INGRESA LA DIRECCIÓN ", orange))
+        print("")
+        leer_ip()
+    
+    sys.stdout.flush()
+    print("")
+
+    if (len(data) == 0):
+        print(stylize(" POR FAVOR INGRESA LOS DATOS DEL ARCHIVO ", orange))
+        print("")
+        leer_archivo()
+
+    sys.stdout.flush()
+    print("")
+    print(stylize(" PUBLICANDO DATOS AL SERVIDOR ", green))
+    print("")
+    total = len(data)
+    i = 1
+    errored = 0
+    success = 0
+    for s in data:
+        pd = { "author": s[1]["name"], "sentence": s[0] }
+        pdn = len(pd["sentence"])
+        pdn = pdn if pdn < 10 else 10
+        
+        status = ""
+        try:
+            print("Enviando a la dirección: {}{}".format(ip, url))
+            print(pd)
+
+            x = requests.post('{}{}'.format(ip, url), json= pd)
+            code = x.status_code
+            recieved_data = x.json()
+            
+            if (code == 200):
+                print(stylize(" OK 200 ", green))
+                status = str(recieved_data)
+                success = success + 1
+            else:
+                print(stylize(" ERROR " + str(code) + " ", red))
+                status = str(recieved_data)
+                errored = errored + 1
+                
+        except Exception as e:
+            print(stylize(" ERROR ", red))
+            status = "Posting... ERROR"
+            if hasattr(e, 'message'):
+                print(stylize(e.message, colored.fg("red_3a")))
+            else:
+                print(stylize(e, colored.fg("red_3a")))
+            errored = errored + 1
+
+        progress(i, total, status)
+
+        i = i + 1
+        time.sleep(0.25)
+
+    sys.stdout.flush()
+    print("")
+    print("")
+    print("")
+    print(stylize(" TODOS LOS DATOS FUERON ENVIADOS ", blue))
+    print("")
+    correct_percent = round(100 * success / total, 2)
+    incorrect_percent = round(100 * errored / total, 2)
+    print(stylize(" ENVIADOS CORRECTAMENTE: {}% ({} de {}) ".format(correct_percent, success, total), green))
+    print("")
+    print(stylize(" ENVIADOS CON ERROR: {}% ({} de {}) ".format(incorrect_percent, errored, total), red))
+
+    print("")
+    Screen().input('Presiona [Enter] para continuar')
+
 def create_menu():
 
     menu = ConsoleMenu("Sentencer", "Asignador de autores y separador de oraciones", exit_option_text="Salir",
@@ -311,25 +418,29 @@ def create_menu():
 
     read_file_item = FunctionItem("Ingresar ruta del archivo", leer_archivo)
     read_ip_item = FunctionItem(
-        "Ingresar dirección del load-balancer", leer_ip)
+        "Ingresar dirección del servidor", leer_ip)
     tbl_show_item = FunctionItem(
         "Mostrar datos recolectados del archivo (como tabla)", table_data)
 
     list_show_item = FunctionItem(
         "Mostrar datos recolectados del archivo (como lista)", list_data)
 
+    post_data_item = FunctionItem(
+        "Publicar datos al servidor", post_data)
+
     menu.append_item(read_file_item)
     menu.append_item(read_ip_item)
     menu.append_item(tbl_show_item)
     menu.append_item(list_show_item)
+    menu.append_item(post_data_item)
 
     menu.start()
     menu.join()
 
-
-
 if __name__ == '__main__':
     for x in range(10):
         author_names.append(names.get_full_name())
-
-    create_menu()
+    try:
+        create_menu()
+    except (KeyboardInterrupt, SystemExit):
+        print(stylize("Gracias por usar el programa.", green))
